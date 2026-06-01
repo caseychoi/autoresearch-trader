92a2c515545e9660cd3c101b8551c3b8433c3a63
kc <kc>
2026-06-01 16:39:04 -0700

    Add optimized algorithm for GOOGL

--- strategies/GOOGL_algo.py
+++ strategies/GOOGL_algo.py
@@ -3,11 +3,11 @@ import numpy as np
 
 def generate_signals(df: pd.DataFrame) -> pd.Series:
     delta = df['Close'].diff()
-    gain = (delta.where(delta > 0, 0)).rolling(window=28).mean()
-    loss = (-delta.where(delta < 0, 0)).rolling(window=28).mean()
+    gain = (delta.where(delta > 0, 0)).rolling(window=11).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=11).mean()
     rs = gain / loss
     rsi = 100 - (100 / (1 + rs))
     signals = pd.Series(0, index=df.index)
-    signals[rsi > 67] = -1
-    signals[rsi < 36] = 1
+    signals[rsi > 74] = -1
+    signals[rsi < 34] = 1
     return signals
