df2e4dafe873ee4a83571fdad98386d79c735f4b
kc <kc>
2026-06-01 16:46:27 -0700

    Add optimized algorithm for GS

--- strategies/GS_algo.py
+++ strategies/GS_algo.py
@@ -5,5 +5,5 @@ def generate_signals(df: pd.DataFrame) -> pd.Series:
     ema_fast = df['Close'].ewm(span=6, adjust=False).mean()
     ema_slow = df['Close'].ewm(span=49, adjust=False).mean()
     macd = ema_fast - ema_slow
-    signal_line = macd.ewm(span=5, adjust=False).mean()
+    signal_line = macd.ewm(span=6, adjust=False).mean()
     return pd.Series(np.where(macd > signal_line, 1, -1), index=df.index)
